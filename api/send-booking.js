58c1ee62d760edf987bd1bc3d471e78b0043a851
Naveen Kumar <91355379+naveenkumar163@users.noreply.github.com>
2026-06-14 01:50:05 +0530

    Update send-booking.js

--- api/send-booking.js
+++ api/send-booking.js
@@ -86,7 +86,7 @@ export default async function handler(req, res) {
   try {
     await resend.emails.send({
       from: 'Booking <onboarding@resend.dev>',
-      to: 'nkuma.021996@gmail.com',
+      to: 'imnaveenkumar.1996@gmail.com',
       subject: '🚖 New Cab Booking - Shree Ganesh',
       html,
     });
